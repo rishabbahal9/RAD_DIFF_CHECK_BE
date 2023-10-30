1fa1cf6e4ed7e7c0a2a8344222de38ecaa64dec7
Rishab Bahal <rishabbahal@gmail.com>
2023-10-30 19:22:56 -0400

    Created a basic api returning formated template text (hardcoded)

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+api=Api(app)
+
+
+class Template(Resource):
+    def get(self):
+        return {"content": """EXAM:
+CT [_laterality_] [Generic Bone], [without or with] IV contrast
+
+CLINICAL HISTORY:
+
+TECHNIQUE:
+Axial images were acquired through the [_laterality_] [generic bone]
+[without or with] IV contrast. Reformatted images were reviewed.
+Dose reduction technique was used including one or more of the following:
+automated exposure control, adjustment of mA and kV according to patient size,
+and/or iterative reconstruction.
+
+COMPARISON:
+None provided.
+
+FINDINGS:
+
+BONES:
+No acute fracture or focal osseous lesion.
+
+JOINTS:
+No dislocation. The joint spaces are normal.
+
+SOFT TISSUES:
+The soft tissues are unremarkable.
+
+IMPRESSION:
+
+1. No acute osseous abnormality.
+"""}
+
+api.add_resource(Template, "/get-report-template")
+
+if __name__ == "__main__":
+    app.run(debug=True)
